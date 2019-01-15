d64c37d83e40aab095215d621fafb025751f5318
Axolotle <nchesnais@mailoo.org>
2019-01-15 18:17:29 +0100

    add some conversion utilities

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,26 @@
 export function hourToString(decimalHour) {
-    let date = new Date(0,0);
+    let date = new Date(Date.UTC(0,0));
     date.setSeconds(decimalHour * 60 * 60);
     return date.toTimeString().slice(0, 8);
 }
 
-export function deg_to_rad(deg) {
+export function degToHourString(deg) {
+    return hourToString(deg / 15);
+}
+
+export function degToRad(deg) {
     return deg * (Math.PI / 180);
 }
 
-export function rad_to_deg(rad) {
+export function radToDeg(rad) {
     return rad * (180 / Math.PI);
+}
+
+export function minToDeg(min) {
+    return min / 60;
+}
+
+export function degToHour(deg) {
+    // deg / (360 / 24)
+    return deg / 15;
 }
